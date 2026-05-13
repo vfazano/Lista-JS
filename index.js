639af47f7a0ef9e9db288cb9fd84d695c1b8d445
Vitor Fazano <vfazano09032007@gmail.com>
2026-05-13 20:10:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('taskInput')
+const botao = document.getElementById('addTaskBtn')
+const lista = document.getElementById ('taskList')
+
+//Funções
+function adicionarTarefa (){
+    const tarefa = input.value
+    const li = document.createElement("li")
+    li
+}
+
+//Eventos 
+botao.addEventListener("click", adicionarTarefa)
+    

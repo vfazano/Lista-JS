e36ef4e0f8b2e00d4359a9fe84284aceeaf6f702
Vitor Fazano <vfazano09032007@gmail.com>
2026-05-13 21:56:06 -0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-const input = document.getElementById('taskInput')
-const botao = document.getElementById('addTaskBtn')
-const lista = document.getElementById ('taskList')
+//Selecionar os elementos HTML
+const input = document.getElementById("taskInput")
+const botao = document.getElementById("addTaskBtn")
+const lista = document.getElementById("taskList")
+const mensagem = document.getElementById("emptyMessage")
 
 //Funções
-function adicionarTarefa (){
-    const tarefa = input.value
-    const li = document.createElement("li")
-    li
+function atualizaMensagemVazia(){
+    if (lista.children.length === 0){
+        mensagem.style.display = "block"
+    }
+    else{
+        mensagem.style.display = "none"
+    }
 }
 
-//Eventos 
-botao.addEventListener("click", adicionarTarefa)
+function adicionarTarefa(){
+    const tarefa = input.value
+
+    if(tarefa == ""){
+        alert("Tarefa inválida. Insira novamente")
+        return
+    }
+
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    span.textContent = tarefa
+
+    const botoes = document.createElement("div")
+    botoes.classList.add("actions")
+
+    const botaoCompletar = document.createElement("button")
+    botaoCompletar.textContent = "OK"
+    botaoCompletar.classList.add("complete-btn")
+
+    const botaoDeletar = document.createElement("button")
+    botaoDeletar.textContent = "X"
+    botaoDeletar.classList.add("delete-btn")
     
+    botaoCompletar.addEventListener("click", () => {
+        li.classList.toggle("completed")
+    })
+    botaoDeletar.addEventListener("click", () => {
+        li.remove()
+        atualizaMensagemVazia()
+    })
+
+    botoes.appendChild(botaoCompletar)
+    botoes.appendChild(botaoDeletar)
+
+    li.appendChild(span)
+    li.appendChild(botoes)
+
+    lista.appendChild(li)
+
+    input.value = "" //Após enviar, deixa o campo vazio
+    atualizaMensagemVazia()
+}
+
+//Eventos
+botao.addEventListener("click", adicionarTarefa)
